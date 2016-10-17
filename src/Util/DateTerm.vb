82fbb33bc7f5b625fda8b8bec557c7a18219627d
istama <night.watch.kc@gmail.com>
2016-10-17 22:13:30 +0900

    create class, DateTerm

---
+++ src/Util/DateTerm.vb
@@ -0,0 +1,190 @@
+﻿
+Option Strict On
+'
+' 日付: 2016/10/17
+'
+Namespace Util
+  
+''' <summary>
+''' 期間を表す構造体。
+''' </summary>
+Public Structure DateTerm
+  ''' 開始日
+  Private ReadOnly min As DateTime
+  ''' 終了日
+  Private ReadOnly max As DateTime
+  ''' このオブジェクトを文字列表現するためのラベル
+  Private ReadOnly _label As String
+  
+  ''' このオブジェクトの期間を１日おきに分割したリスト
+  Private dailyTermList As List(Of DateTerm)
+  ''' このオブジェクトの期間を１週間おきに分割したリスト
+  Private weeklyTermList As List(Of DateTerm)
+  ''' このオブジェクトの期間を１ヶ月おきに分割したリスト
+  Private monthlyTermList As List(Of DateTerm)
+  
+  Public Sub New(min As DateTime, max As DateTime, Optional label As String="")
+    Me.min = min
+    Me.max = max
+    Me._label = label
+  End Sub
+  
+  ''' <summary>
+  ''' 期間の開始日を返す。
+  ''' </summary>
+  Public Function BeginDate() As DateTime
+    Return Me.min
+  End Function
+  
+  ''' <summary>
+  ''' 期間の終了日を返す。
+  ''' </summary>
+  Public Function EndDate() As DateTime
+    Return Me.max
+  End Function
+  
+  ''' <summary>
+  ''' ラベルを返す。
+  ''' </summary>
+  Public Function Label() As String
+    Return Me._label
+  End Function
+  
+  ''' <summary>
+  ''' 期間を１日おきに分割したリストを返す。
+  ''' </summary>
+  Public Function DailyTerms() As List(Of DateTerm)
+    If Me.dailyTermList Is Nothing Then
+      Return DailyTerms(Function(d) String.Empty)
+    Else
+      Return Me.dailyTermList
+    End If
+  End Function
+  
+  ''' <summary>
+  ''' 期間を１日おきに分割したリストを返す。
+  ''' 引数にはこの各日付のラベルを返す関数。
+  ''' </summary>
+  Public Function DailyTerms(f As Func(Of DateTime, String)) As List(Of DateTerm)
+    If f Is Nothing Then Throw New ArgumentNullException("f is null")
+    
+    Dim l As New List(Of DateTerm)
+      
+    Dim d As DateTime = Me.BeginDate
+    While d <= Me.EndDate
+      l.Add(New DateTerm(d, d, f(d)))
+      d = d.AddDays(1)
+    End While
+    
+    Me.dailyTermList = l
+  
+    Return Me.dailyTermList
+  End Function
+  
+  ''' <summary>
+  ''' 期間を１週間おきに分割したリストを返す。
+  ''' </summary>
+  Public Function WeeklyTerms() As List(Of DateTerm)
+    If Me.WeeklyTerms Is Nothing Then
+      Return WeeklyTerms(DayOfWeek.Saturday, Function(b, e) String.Empty)
+    Else
+      Return Me.weeklyTermList
+    End If
+  End Function
+  
+  ''' <summary>
+  ''' 期間を１週間おきに分割したリストを返す。
+  ''' 引数にはこの各日付のラベルを返す関数。
+  ''' </summary>
+  Public Function WeeklyTerms(weekend As DayOfWeek, f As Func(Of DateTime, DateTime, String)) As List(Of DateTerm)
+    If f Is Nothing Then Throw New ArgumentNullException("f is null")
+  
+    Dim l As New List(Of DateTerm)
+    
+    Dim beginDate As DateTime = Me.BeginDate   ' 週の開始日
+    Dim weekCntInMonth As Integer = 1  		' １ヶ月の中での週をカウント
+    
+    While beginDate <= Me.EndDate
+      ' 週の開始日と終了日のセットを生成
+      Dim endDate As DateTime = DateUtils.GetDateOfNextWeekDay(beginDate, weekend)
+      If endDate > Me.EndDate Then
+        endDate = Me.EndDate
+      End If
+      
+      l.Add(New DateTerm(beginDate, endDate, f(beginDate, endDate)))
+      
+      ' 週の開始日を更新
+      beginDate = endDate.AddDays(1)
+    End While
+    
+    Me.weeklyTermList = l
+'			' この週を表す文字列を生成
+'			Dim label As String
+'			' 週の終了日が翌月にまたがない場合
+'			If endDate.Month = beginDate.Month Then
+'				label = String.Format("{0:00}月第{1}週", beginDate.Month, weekCntInMonth.ToString)
+'				' 終了日が月末日の場合
+'				If endDate.Day = DateTime.DaysInMonth(endDate.Year, endDate.Month) Then
+'					weekCntInMonth = 1 ' 翌月の第１週からカウントを開始
+'				Else
+'					weekCntInMonth += 1 ' 週のカウントを加算
+'				End If
+'			Else
+'				label = String.Format("{0:00}月第{1}週/{2:00}月第1週", beginDate.Month, weekCntInMonth.ToString, endDate.Month)
+'				weekCntInMonth = 2	' 翌月の第２週からカウントを開始
+'			End If
+    
+    Return Me.WeeklyTermList
+  End Function
+  
+  ''' <summary>
+  ''' 期間を１ヶ月おきに分割したリストを返す。
+  ''' </summary>
+  Public Function MonthlyTerms() As List(Of DateTerm)
+    If Me.monthlyTermList Is Nothing Then
+      Return monthlyTerms(Function(b, e) String.Empty)
+    Else
+      Return Me.monthlyTermList
+    End If
+  End Function
+  
+  ''' <summary>
+  ''' 期間を１ヶ月おきに分割したリストを返す。
+  ''' 引数にはこの各日付のラベルを返す関数。
+  ''' </summary>
+  Public Function MonthlyTerms(f As Func(Of DateTime, DateTime, String)) As List(Of DateTerm)
+    If f Is Nothing Then Throw New ArgumentNullException("f is null")
+  
+    Dim l As New List(Of DateTerm)
+    
+    Dim min = Me.BeginDate()
+    Dim max = Me.EndDate()
+    
+    DateUtils.GetDateListOfEveryMonth(min, max).ForEach(
+      Sub(d)
+        ' 月の開始日と月末日のセットを作成
+        Dim begin As DateTime = d
+        If min > begin Then
+          begin = min
+        End If
+        
+        Dim _end As New DateTime(d.Year, d.Month, DateTime.DaysInMonth(d.Year, d.Month))
+        If max < _end Then
+          _end = max
+        End If
+        
+        l.Add(New DateTerm(begin, _end, f(begin, _end)))
+      End Sub)
+    
+    Me.monthlyTermList = l
+  
+    Return Me.monthlyTermList
+  End Function
+  
+  Public Overrides Function ToString As String
+    Return String.Format("{0} - {1}", Me.BeginDate().ToString, Me.EndDate().ToString)
+  End Function
+  
+End Structure
+
+End Namespace
---
+++ test/Util/TestDateTerm.vb
@@ -0,0 +1,88 @@
+﻿'
+' 日付: 2016/10/17
+'
+Imports NUnit.Framework
+
+Imports Common.Util
+
+<TestFixture> _
+Public Class TestDateTerm
+  
+  <Test> _
+  Public Sub TestConstructor
+    Dim min As New DateTime(2010, 1, 1)
+    Dim max As New DateTime(2015, 12, 31)
+    Dim str As String = "abc"
+    Dim t As New DateTerm(min, max, str)
+    
+    Assert.AreEqual(min, t.BeginDate)
+    Assert.AreEqual(max, t.EndDate)
+    Assert.AreEqual(str, t.Label)
+  End Sub
+  
+  <Test>
+  Public Sub TestDailyTerms
+    Dim t As New DateTerm(New DateTime(2016, 09, 30), New DateTime(2016, 11, 1))
+    Dim l As List(Of DateTerm) = t.DailyTerms(Function(d) String.Format("{0}月{1}日", d.Month, d.Day))
+    
+    Assert.AreEqual(33, l.Count)
+    AssertDailyTerm(2016,  9, 30, l(0))
+    AssertDailyTerm(2016, 10,  1, l(1))
+    AssertDailyTerm(2016, 10, 31, l(31))
+    AssertDailyTerm(2016, 11,  1, l(32))
+  End Sub
+  
+  Private Sub AssertDailyTerm(y As Integer, m As Integer, d As Integer, term As DateTerm)
+    AssertDate(y, m, d, term.BeginDate)
+    AssertDate(y, m, d, term.EndDate)
+    Assert.AreEqual(String.Format("{0}月{1}日", m, d), term.Label)
+  End Sub
+  
+  <Test>
+  Public Sub TestWeeklyTerm
+    Dim t As New DateTerm(New DateTime(2016, 12, 3), New DateTime(2016, 12, 31))
+    Dim l As List(Of DateTerm) = t.WeeklyTerms(DayOfWeek.Saturday, Function(b, e) String.Format("{0}日-{1}日", b.Day, e.Day))
+    
+    Assert.AreEqual(5, l.Count)
+    AssertDate(2016, 12,  3, l(0).BeginDate)
+    AssertDate(2016, 12,  3, l(0).EndDate)
+    Assert.AreEqual("3日-3日", l(0).Label)
+    
+    AssertDate(2016, 12,  4, l(1).BeginDate)
+    AssertDate(2016, 12, 10, l(1).EndDate)
+    
+    AssertDate(2016, 12, 25, l(4).BeginDate)
+    AssertDate(2016, 12, 31, l(4).EndDate)
+    
+    Dim t2 As New DateTerm(New DateTime(2016, 12, 31), New DateTime(2017, 1, 1))
+    Dim l2 As List(Of DateTerm) = t2.WeeklyTerms(DayOfWeek.Saturday, Function(b, e) String.Empty)
+    
+    Assert.AreEqual(2, l2.Count)
+    AssertDate(2016, 12, 31, l2(0).BeginDate)
+    AssertDate(2016, 12, 31, l2(0).EndDate)
+    
+    AssertDate(2017,  1,  1, l2(1).BeginDate)
+    AssertDate(2017,  1,  1, l2(1).EndDate)
+  End Sub
+  
+  <Test>
+  Public Sub TestMonthlyTerms()
+    Dim t As New DateTerm(New DateTime(2016, 11, 30), New DateTime(2017, 1, 1))
+    Dim l As List(Of DateTerm) = t.MonthlyTerms(Function(b, e) String.Format("{0}月{1}日-{2}日", b.Month, b.Day, e.Day))
+    
+    Assert.AreEqual(3, l.Count)
+    AssertDate(2016, 11, 30, l(0).BeginDate)
+    AssertDate(2016, 11, 30, l(0).EndDate)
+    Assert.AreEqual("11月30日-30日", l(0).Label)
+    
+    AssertDate(2016, 12,  1, l(1).BeginDate)
+    AssertDate(2016, 12, 31, l(1).EndDate)
+        
+    AssertDate(2017,  1,  1, l(2).BeginDate)
+    AssertDate(2017,  1,  1, l(2).EndDate)
+  End Sub
+  
+  Private Sub AssertDate(y As Integer, m As Integer, d As Integer, _date As DateTime)
+    Assert.AreEqual(New DateTime(y, m, d), _date)
+  End Sub
+End Class
