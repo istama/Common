4caad8d482d0ca2931aacf807b456ef62eff9a96
istama <night.watch.kc@gmail.com>
2016-10-24 16:20:13 +0900

    modify moethd, Log.out()
    modified Log.out() to a thread-safe.

--- src/IO/Log.vb
+++ src/IO/Log.vb
@@ -1,10 +1,5 @@
 ﻿'
-' SharpDevelopによって生成
-' ユーザ: Blue
 ' 日付: 2016/09/26
-' 時刻: 21:59
-' 
-' このテンプレートを変更する場合「ツール→オプション→コーディング→標準ヘッダの編集」
 '
 Namespace IO
   
@@ -13,13 +8,15 @@ Public Class Log
   
   Public Shared Sub SetFilePath(path As String)
     file = New TextFile(path, System.Text.Encoding.UTF8)
-    file.Create
+    file.Close
   End Sub
   
   Public Shared Sub out(text As String)
-    If file IsNot Nothing Then
-      file.Append(text)
-    End If
+    SyncLock file
+      If file IsNot Nothing Then
+        file.Append(text)
+      End If
+    End SyncLock
   End Sub
 End Class
 
