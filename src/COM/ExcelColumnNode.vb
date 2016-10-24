09db9bc3cd664713f3d514765f5049b970c1b821
istama <night.watch.kc@gmail.com>
2016-10-24 18:29:46 +0900

    create class, ExcelColumnNode

---
+++ src/COM/ExcelColumnNode.vb
@@ -0,0 +1,51 @@
+﻿'
+' 日付: 2016/10/18
+'
+Namespace COM
+  
+'''' <summary>
+'''' 読み込むExcelの列とその列名を持つクラス。
+'''' 複数のこのクラスのインスタンスを木構造に結合することができる。
+'''' 列に対して処理を行うときに、子ノードの処理を親ノードの状態に依存させることなどができる。
+'''' </summary>
+Public Structure ExcelColumnNode
+  Private ReadOnly name As String
+  Private ReadOnly col As String
+  
+  Private ReadOnly notContaindeToDataTable As Boolean
+  
+  Private ReadOnly childs As List(Of ExcelColumnNode)
+  
+  Public Sub New(col As String, Optional name As String="", Optional notContainedToDataTable As Boolean=False)
+    If col Is Nothing Then Throw New ArgumentNullException("col is null")
+    If Not Cell.ValidColumn(col) Then Throw New ArgumentException("col is invalid value / " & col)
+    
+    Me.col = col
+    Me.name = name
+    Me.notContaindeToDataTable = notContaindeToDataTable
+    Me.childs = New List(Of ExcelColumnNode)
+  End Sub
+  
+  Public Function GetName() As String
+    Return Me.name
+  End Function
+  
+  Public Function GetCol() As String
+    Return Me.col
+  End Function
+  
+  Public Function ContainedToDataTable() As Boolean
+    Return Not Me.notContaindeToDataTable
+  End Function
+  
+  Public Function GetChilds() As List(Of ExcelColumnNode)
+    Return New List(Of ExcelColumnNode)(childs.ToArray)
+  End Function
+  
+  Public Sub AddChild(node As ExcelColumnNode)
+    Me.childs.Add(node)
+  End Sub
+  
+End Structure
+
+End Namespace
---
+++ test/COM/TestExcelColumnNode.vb
@@ -0,0 +1,39 @@
+﻿'
+' SharpDevelopによって生成
+' ユーザ: Blue
+' 日付: 2016/10/18
+' 時刻: 12:41
+' 
+' このテンプレートを変更する場合「ツール→オプション→コーディング→標準ヘッダの編集」
+'
+Imports NUnit.Framework
+Imports Common.COM
+
+<TestFixture> _
+Public Class TestExcelColumnNode
+  <Test> _
+  Public Sub TestConstructor
+    Dim n As New ExcelColumnNode("A", "col1")
+    
+    Assert.AreEqual("A", n.GetCol)
+    Assert.AreEqual("col1", n.GetName)
+    Assert.AreEqual(0, n.GetChilds.Count)
+  End Sub
+  
+  <Test>
+  Public Sub TestAddChild
+    Dim n1 As New ExcelColumnNode("A", "col1")
+    Dim n2 As New ExcelColumnNode("B", "col2")
+    Dim n3 As New ExcelColumnNode("C", "col3")
+    Dim n4 As New ExcelColumnNode("D", "col4")
+    
+    n1.AddChild(n2)
+    n2.AddChild(n3)
+    n2.AddChild(n4)
+    
+    Assert.AreEqual("A", n1.GetCol)
+    Assert.AreEqual("B", n1.GetChilds(0).GetCol)
+    Assert.AreEqual("C", n1.GetChilds(0).GetChilds(0).GetCol)
+    Assert.AreEqual("D", n1.GetChilds(0).GetChilds(1).GetCol)
+  End Sub
+End Class
