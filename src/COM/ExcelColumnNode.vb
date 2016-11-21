fb4da2408d24e06794d7b1baa7ef280774c75dc7
istama <night.watch.kc@gmail.com>
2016-11-21 18:41:27 +0900

    modify ExcelColumnNode: reverse flag field

--- src/COM/ExcelColumnNode.vb
+++ src/COM/ExcelColumnNode.vb
@@ -13,11 +13,11 @@ Public Structure ExcelColumnNode
   Private ReadOnly col As String
   Private ReadOnly type As Type
   
-  Private ReadOnly notContainedToDataTable As Boolean
+  Private ReadOnly _containedToDataTable As Boolean
   
   Private ReadOnly childs As List(Of ExcelColumnNode)
   
-  Public Sub New(col As String, Optional name As String="", Optional type As Type=Nothing, Optional notContainedToDataTable As Boolean=False)
+  Public Sub New(col As String, Optional name As String="", Optional type As Type=Nothing, Optional containedToDataTable As Boolean=True)
     If col Is Nothing Then Throw New ArgumentNullException("col is null")
     If Not Cell.ValidColumn(col) Then Throw New ArgumentException("col is invalid value / " & col)
     
@@ -28,7 +28,7 @@ Public Structure ExcelColumnNode
     Me.col = col
     Me.name = name
     Me.type = type
-    Me.notContainedToDataTable = notContainedToDataTable
+    Me._containedToDataTable = containedToDataTable
     Me.childs = New List(Of ExcelColumnNode)
   End Sub
   
@@ -41,7 +41,7 @@ Public Structure ExcelColumnNode
   End Function
   
   Public Function ContainedToDataTable() As Boolean
-    Return Me.notContainedToDataTable = False
+    Return Me._containedToDataTable
   End Function
   
   Public Function GetChilds() As List(Of ExcelColumnNode)
