ae8b7970dbd162a946ff3fdee44a99f270655b88
istama <night.watch.kc@gmail.com>
2016-11-23 23:30:57 +0900

    create Module, DataRowCollectionExtensions

---
+++ src/Extensions/DataRowCollectionExtensions.vb
@@ -0,0 +1,34 @@
+﻿'
+' 日付: 2016/11/23
+'
+Imports System.Data
+
+Namespace Extensions
+
+Public Module DataRowCollectionExtensions
+  
+  <System.Runtime.CompilerServices.ExtensionAttribute()>
+  Public Sub ForEach(collection As DataRowCollection, f As Action(Of DataRow))
+    For Each row As DataRow In collection
+      f(row)
+    Next
+  End Sub
+  
+  <System.Runtime.CompilerServices.ExtensionAttribute()>
+  Public Iterator Function Convert(OF T)(collection As DataRowCollection, f As Func(Of DataRow, T)) As IEnumerable(Of T)
+    For Each row As DataRow In collection
+      Yield f(row)
+    Next
+  End Function
+  
+  <System.Runtime.CompilerServices.ExtensionAttribute()>
+  Public Iterator Function Where(collection As DataRowCollection, f As Func(Of DataRow, Boolean)) As IEnumerable(Of DataRow)
+    For Each row As DataRow In collection
+      If f(row) Then
+        Yield row
+      End If
+    Next
+  End Function
+End Module
+
+End Namespace
