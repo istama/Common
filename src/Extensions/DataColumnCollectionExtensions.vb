3811c97097f825a396ce5dcc0d0122a0eaff551c
istama <night.watch.kc@gmail.com>
2016-11-26 08:31:19 +0900

    create class, DataColumnCollectionExtensions

---
+++ src/Extensions/DataColumnCollectionExtensions.vb
@@ -0,0 +1,18 @@
+﻿'
+' 日付: 2016/11/25
+'
+Imports System.Data
+
+Namespace Extensions
+  
+Public Module DataColumnCollectionExtensions
+  
+  <System.Runtime.CompilerServices.ExtensionAttribute()>
+  Public Iterator Function ToEnumerable(collection As DataColumnCollection) As IEnumerable(Of DataColumn)
+    For Each col As DataColumn In collection
+      Yield col
+    Next
+  End Function
+End Module
+
+End Namespace
